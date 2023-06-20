536e4945271c4013a86dc34905276c064e50b2b5
Chiara888 <carolin@family-klose.de>
2023-06-20 16:52:55 +0200

    status quo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 import Header from "./components/Header";
 import Footer from './components/Footer';
 import Main from "./components/Main";
+
+import './App.css';
+
 
 function App() {
   return (
@@ -11,20 +11,6 @@ function App() {
       <Header/>
       <Main/>
       <Footer/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-function Header(){
-    return(
-        <>
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="Header">
             <h1>This is the Header</h1>
-        </>
+        </div>
     );
 }
 
