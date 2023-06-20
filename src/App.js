e470cdf3f182c344b5c686a8df60826218a9510c
Chiara888 <carolin@family-klose.de>
2023-06-20 12:50:34 +0200

    Setup popcorn time done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from "./components/Header";
+import Footer from './components/Footer';
+import Main from "./components/Main";
+
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
